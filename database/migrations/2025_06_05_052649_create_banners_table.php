216a46259ee3a43f30362792d31be4f927b2e574
kaho43 <kaho.shimizu0923@gmail.com>
2025-06-05 14:49:44 +0900

    curriculums,delivery_times,bannersマイグレーションファイル追加

---
+++ database/migrations/2025_06_05_052649_create_banners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('image',255)->nullable(false);//バナー画像
+            $table->timestamps()->nullable(false);//created_at,updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+};
