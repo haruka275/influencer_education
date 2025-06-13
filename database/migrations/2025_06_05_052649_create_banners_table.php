35b5a6cc2b996d2b91bd95fa8f0952da57ee8ccb
kemichael <michael.kiya.bz@gmail.com>
2025-06-13 22:13:29 +0900

    fetch:feature/kaho

--- database/migrations/2025_06_05_052649_create_banners_table.php
+++ database/migrations/2025_06_05_052649_create_banners_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('image',255)->nullable(false);//バナー画像
+            $table->timestamps();//created_at,updated_at
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('banners');
     }
 };
